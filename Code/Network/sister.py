24bf5b102516928ba74629b4b87754bb41343bdd
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-31 18:02:23 +0100

    [FEAT] Deeper sister network

--- Code/Network/sister.py
+++ Code/Network/sister.py
@@ -5,28 +5,49 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Flatten
 
 
-def SisterNetwork(input_shape, latent_dim):
+def SisterNetwork(shape, filters=(4, 8, 16, 32, 64, 128), latent = 64):
 
-    # Specify the inputs for the feature extractor network
-    inputs = Input(input_shape)
+    """ Sister network used for feature extraction from images. It is basically
+    just a CNN """
 
-    # First set of CONV => RELU => POOL => DROPOUT layers
-    x = Conv2D(64, (2, 2), padding="same", activation="relu")(inputs)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Dropout(0.3)(x)
+        # Instantiate a Keras tensor (it should be a n_xpixel x n_ypixel x n_channel tensor)
+    input = Input(shape=shape)
 
-    # Second set of CONV => RELU => POOL => DROPOUT layers
-    x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-    x = MaxPooling2D(pool_size=2)(x)
-    x = Dropout(0.3)(x)
+    # At first, the tensor is simply the input
+    x = input
 
-    # Prepare the final outputs
-    pooled_output = GlobalAveragePooling2D()(x)
-    outputs = Dense(latent_dim)(pooled_output)
+    # Loop over the number of filters to construct CNN recursively
+    for f in filters:
+
+        # Apply 2D convolution with ReLU activation function
+        x = Conv2D(f, kernel_size=(3, 3), activation="relu", padding="same")(x)
+
+        # Apply batch normalisation (specify axis = -1 if assuming TensorFlow/channels-last ordering)
+        x = BatchNormalization(axis=-1)(x)
+
+        # Apply Max Pooling
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    # Add drop-out layer
+    x = Dropout(0.2)(x)
+
+    # Global average pooling
+    x = GlobalAveragePooling2D()(x)
+
+    # Flatten the tensor before applying the dense layer
+    x = Flatten()(x)
+
+    # Apply dense layer (units = dimensionality of the output layer)
+    x = Dense(units=latent, activation="relu")(x)
+
+    # Apply batch normalisation
+    output = BatchNormalization(axis=-1)(x)
 
     # Build the model
-    sister_net = Model(inputs, outputs)
+    sister_net = Model(input, output)
 
     return sister_net
