b9239d1db06d534d520dc2b65a5cb57049e9bb36
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-31 18:32:17 +0100

    [ADD] Add conv options

--- Code/Parser/parser.py
+++ Code/Parser/parser.py
@@ -16,7 +16,7 @@ def parse():
         default=48,
         nargs="?",
         type=int,
-        help="Integer for latent space dimension"
+        help="Integer for latent space dimension",
     )
 
     parser.add_argument(
@@ -25,7 +25,7 @@ def parse():
         default=32,
         nargs="?",
         type=int,
-        help="Integer for batch size"
+        help="Integer for batch size",
     )
 
     parser.add_argument(
@@ -34,7 +34,7 @@ def parse():
         default=100,
         nargs="?",
         type=int,
-        help="Integer for training epochs"
+        help="Integer for training epochs",
     )
 
     parser.add_argument(
@@ -43,9 +43,46 @@ def parse():
         default=0.2,
         nargs="?",
         type=float,
-        help="Float for dropout percentage at the end of feature extractor network"
+        help="Float for dropout percentage at the end of feature extractor network",
     )
-   
+
+    parser.add_argument(
+        "--kernel_size_x",
+        const=3,
+        default=3,
+        nargs="?",
+        type=float,
+        help="Integer for x-dimension of convolutional kernel",
+    )
+
+    parser.add_argument(
+        "--kernel_size_y",
+        const=3,
+        default=3,
+        nargs="?",
+        type=float,
+        help="Integer for y-dimension of convolutional kernel",
+    )
+
+    parser.add_argument(
+        "--pool_size_x",
+        const=2,
+        default=2,
+        nargs="?",
+        type=float,
+        help="Integer for x-dimension of pooling kernel",
+    )
+
+    parser.add_argument(
+        "--pool_size_y",
+        const=2,
+        default=2,
+        nargs="?",
+        type=float,
+        help="Integer for y-dimension of  pooling kernel",
+    )
+
+
     # Parse arguments
     args = parser.parse_args()
 
