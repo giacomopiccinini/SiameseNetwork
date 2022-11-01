3a3003c96e3ad320a457c56f7006282afc7b0287
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-01 13:28:18 +0100

    [FIX] Simplify label class

--- Code/Classes/Label.py
+++ Code/Classes/Label.py
@@ -1,35 +1,11 @@
-from pathlib import Path
-from yaml import safe_load
-
-
 class Label:
 
     """Load labels"""
 
-    def __init__(self, path):
+    def __init__(self, image, label):
 
         """Constructor for Label class"""
 
-        self.path = path
-        self.label = self.read()
+        self.image = image
+        self.label = label
 
-    def read(self):
-
-        """Read labels"""
-
-        # Retrieve path
-        path = Path(self.path)
-
-        # Retrieve extension
-        extension = path.suffix
-
-        # Read label
-        with open(path, "r") as file:
-
-            if extension == ".yaml":
-                label = safe_load(file)
-
-            if extension == ".txt":
-                label = file.readline()
-
-        return label
