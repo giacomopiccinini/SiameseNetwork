280206e29dd24d5fe2c57560496b3349c66c68c9
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-01 15:23:54 +0100

    [ADD] Add testing

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import logging
 
 from Code.Parser.parser import parse
 
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 from Code.Modules.A_split import split
 from Code.Modules.B_prepare import prepare
-from Code.Modules.C_train import train
-from Code.Modules.D_test import test
+from Code.Modules.C_pair import pair_dataset
+from Code.Modules.D_train import train
+from Code.Modules.E_test import test
 
 if __name__ == "__main__":
 
@@ -16,13 +21,34 @@ if __name__ == "__main__":
 
     logging.info("Loading datasets")
     train_set, validation_set, test_set = split(args["Split"])
-    shape = train_set.shape
 
     logging.info("Preparing network")
-    Siamese = prepare(args=args, shape=shape)
+    Siamese = prepare(args=args, shape=train_set.shape)
+
+    logging.info("Pairing datasets")
+    (
+        paired_train_images,
+        paired_train_labels,
+        paired_validation_images,
+        paired_validation_labels,
+        paired_test_images,
+        paired_test_labels,
+    ) = pair_dataset(train_set, validation_set, test_set)
 
     logging.info("Training network")
-    experiment = train(Siamese, train_set, validation_set, args)
+    experiment = train(
+        Siamese,
+        paired_train_images,
+        paired_train_labels,
+        paired_validation_images,
+        paired_validation_labels,
+        args,
+    )
 
     logging.info("Testing network")
-    test(Siamese, experiment, test_set)
+    test(
+        Siamese,
+        experiment,
+        paired_test_images,
+        paired_test_labels,
+    )
